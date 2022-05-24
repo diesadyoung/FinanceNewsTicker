e211b1acdcaae926a84256aec3ae03b095399c02
Nick <nickkalashnikov2000@gmail.com>
2022-05-24 17:42:25 +0300

    Added API KEYs for project

--- FinanceNewsTicker/Program.cs
+++ FinanceNewsTicker/Program.cs
@@ -1,7 +1,10 @@
+using FinanceNewsTicker.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<INewsService, NewsService>();
 
 var app = builder.Build();
 
--- FinanceNewsTicker/Services/INewsService.cs
+++ FinanceNewsTicker/Services/INewsService.cs
@@ -2,5 +2,34 @@
 {
     public interface INewsService
     {
+        void GetFinanceNews();
+    }
+
+    public class NewsService : INewsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public NewsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void GetFinanceNews()
+        {
+            string apiKey = _configuration.GetValue<string>("API_KEY");
+            string baseUrl = _configuration.GetValue<string>("API_URL");
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+
+                HttpResponseMessage response = client.GetAsync("?apiKey=" + apiKey).Result;
+
+                if(response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
     }
 }
